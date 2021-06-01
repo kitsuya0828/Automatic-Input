eebecad014c6a6a51edead17841c33ef9a53df6e
Kitsuya0828 <60843722+Kitsuya0828@users.noreply.github.com>
2021-06-01 22:19:55 +0900

    Create p2_header.h

---
+++ problems/ppa_extra_h/p2_header.h
@@ -0,0 +1,42 @@
+#ifndef _P2_HEADER_H_
+#define _P2_HEADER_H_
+
+void func_swap_(    // 配列内の2つの要素を交換し，操作をSTDOUTに出力
+  int* const array,   // 要素が交換される配列
+  const int pos_a,    // 1番目の交換対象の操作番号
+  const int pos_b    // 2番目の交換対象の操作番号
+);
+
+int comp_(    // 配列内の2つの要素を比較する関数
+  int* const array,    // 要素の比較を行う配列
+  const int pos_a,    // 1つ目の比較対象の要素番号
+  const int pos_b    // 2つ目の比較対象の要素番号
+);
+
+void print_array_(    // 配列をSTDOUTに表示する関数
+  const int* const array,    // 表示する配列
+  const int N    // 配列の要素数
+);
+
+int* func_mallocation_(    // 配列の動的メモリ確保をおこなう関数
+  const int N    // 配列の要素数
+);
+
+int reverse_array(    // 配列を逆順に並べ替える関数
+  int* array,    // 動的に確保された，並べ替えされる配列
+  int N    // 入力データの個数
+);
+
+int* func_copy_val_(
+  int* const array,
+  const int pos_a,
+  const int pos_b
+);
+
+void print_array_range_(
+  const int* const array,
+  const int N,
+  const int p, const int r
+);
+
+#endif // _P2_HEADER_H_
